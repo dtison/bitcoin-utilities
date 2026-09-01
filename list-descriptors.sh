e7f3b886708084e25263bca35e4f34e734a089bb
David Ison <admin@dtison.net>
2026-09-01 01:15:47 -0400

    Implements rpc.conf support in list-descriptors.sh

--- list-descriptors.sh
+++ list-descriptors.sh
@@ -17,9 +17,13 @@ DEBUG=0
 USAGE_STRING="[-w] wallet [-p] Bitcoin path [-P] passphrase  [-h] help"
 HandleArguments "$@"
 
-# Fake for now:
-CONF="-conf=$(pwd)/default.conf"
-CONF=""
+# Support rpc via rpc.conf if file exists
+if [ -f rpc.conf ]
+then
+	CONF="-conf=$(pwd)/rpc.conf"
+else
+	CONF=""
+fi
 
 ## Check if wallet is loaded, and load if not
 if ! ${BITCOIN_PATH}/bitcoin-cli ${CONF} listwallets\
